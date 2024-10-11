d68cf54abfbaa54ebeafc1505dbcd1ab869c575c
davidfurore <d.huson@furore.com>
2024-10-11 13:24:30 +0200

    navbar wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,26 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useRef, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 const Navbar = () => {
+  const [selectedWork, setSelectedWork] = useState("");
+  const location = useLocation();
+  const navStyle = useRef();
+  console.log("sdsd", location.pathname);
+
   const handelClick = (item) => {
-    console.log(item);
+    if (item === "music") {
+      navStyle.current = "translate-y-0";
+    } else if (item === "film") {
+      navStyle.current = "-translate-y-6";
+    } else if (item === "other") {
+      navStyle.current = "-translate-y-12";
+    }
+
+    setSelectedWork(item);
+    console.log(selectedWork);
     const element = document.getElementById(item);
-    console.log(item);
-    element.scrollIntoView(true);
+    if (element !== null) {
+      element.scrollIntoView(true, { smooth: true });
+    }
   };
 
   return (
@@ -18,27 +33,35 @@ const Navbar = () => {
           <Link className="hover:text-gray-500" to="/works">
             Works
           </Link>
-          <span className="absolute top-[10px] bg-red-700 ml-4 w-1/4 h-0.5 rounded-md" />
-          <ul className="absolute left-[100px] top-0">
-            <li
-              onClick={() => handelClick("music")}
-              className="pb-1 hover:text-gray-500 cursor-pointer"
-            >
-              Music
-            </li>
-            <li
-              onClick={() => handelClick("film")}
-              className="pb-1 hover:text-gray-500 cursor-pointer"
-            >
-              Film
-            </li>
-            <li
-              onClick={() => handelClick("other")}
-              className="pb-1 hover:text-gray-500 cursor-pointer"
-            >
-              Other
-            </li>
-          </ul>
+          {location.pathname === "/works" ? (
+            <div className="transition-all">
+              <span className="absolute top-[10px] bg-red-700 ml-20 w-[20px] h-0.5 rounded-md tra" />
+              <ul
+                className={`${navStyle.current} ml-3 absolute left-24 top-0 transition duration-300`}
+              >
+                <li
+                  onClick={() => handelClick("music")}
+                  className="pb-1 hover:text-gray-500 cursor-pointer"
+                >
+                  Music
+                </li>
+                <li
+                  onClick={() => handelClick("film")}
+                  className="pb-1 hover:text-gray-500 cursor-pointer"
+                >
+                  Film
+                </li>
+                <li
+                  onClick={() => handelClick("other")}
+                  className="pb-1 hover:text-gray-500 cursor-pointer"
+                >
+                  Other
+                </li>
+              </ul>
+            </div>
+          ) : (
+            ""
+          )}
         </li>
         <li className="hover:text-gray-500 pb-1">
           <Link to="/contact">Contact</Link>
--- src/layouts/RootLayout.js
+++ src/layouts/RootLayout.js
@@ -7,7 +7,7 @@ const RootLayout = () => {
     <div className="container mx-auto px-16 pt-16">
       <div className="text-center lg:text-start lg:flex h-full">
         <header>
-          <div class="pb-12">
+          <div className="pb-12">
             <h1 className="font-medium text-xl">David Huson</h1>
             <p className="text-xs"> Flamenco Guitarist & Producer</p>
           </div>
