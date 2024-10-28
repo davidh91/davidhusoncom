93cfd413677b1a339e528e8a7e7508dd49c906bc
davidh91 <d.huson@gmail.com>
2024-10-28 14:39:13 +0100

    comment gh-pages

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ export const TriggerContext = createContext();
 
 root.render(
   <React.StrictMode>
+    {/* Make sure basename matches your github repo to work with gh-pages */}
     <BrowserRouter basename="/davidhusoncom">
       <Routes>
         <Route path="" element={<RootLayout />}>
